267d6f229d753a30607f4be273d07d50b0ccf7d5
ENEMNM <smyh4592@naver.com>
2017-09-22 21:53:49 +0900

    stateView

--- src/app/app.mainview.ts
+++ src/app/app.mainview.ts
@@ -5,5 +5,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.mainview.css']
 })
 export class MainView {
-  title = 'main';
+  date: Date;
+  tooltip1: number = 1;
+  tooltip2: number = 1;
+
+  constructor() {
+    this.date = new Date();
+  }
+
+  tooltipToggle(indexNum) {
+    if(indexNum === 1) {
+      this.tooltip1 != this.tooltip1;
+    }
+    if(indexNum === 2) {
+      this.tooltip2 != this.tooltip2;
+    }
+  }
 }
